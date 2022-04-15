563f0bfa96af723614a378dedf7c5a65aba54e83
league <league@imac-16.attlocal.net>
2022-04-15 16:53:26 -0700

    StringsCannotAdd, RollerCoaster, VotingBooth

---
+++ src/_03_string_conversion/_4_voting_booth/VotingBooth.java
@@ -0,0 +1,12 @@
+package _03_string_conversion._4_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class VotingBooth {
+public static void main(String[] args) {
+	
+	String age = JOptionPane.showInputDialog(null, "enter your age");	
+	int numberAge = Integer.parseInt(age);
+	
+}
+}

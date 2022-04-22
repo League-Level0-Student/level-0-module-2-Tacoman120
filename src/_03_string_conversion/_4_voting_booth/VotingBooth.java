de3b48ed10521bb8d24e63085108743f4e3aa667
league <league@imac-16.attlocal.net>
2022-04-22 16:50:06 -0700

    spotTheError,Voting booth

--- src/_03_string_conversion/_4_voting_booth/VotingBooth.java
+++ src/_03_string_conversion/_4_voting_booth/VotingBooth.java
@@ -6,7 +6,11 @@ public class VotingBooth {
 public static void main(String[] args) {
 	
 	String age = JOptionPane.showInputDialog(null, "enter your age");	
-	int numberAge = Integer.parseInt(age);
-	
+	int numberAge = Integer.parseInt(age); 
+	if(numberAge >= 18) {
+		String ask = JOptionPane.showInputDialog(null, "who do you think the next president should be");	
+	}else{
+		 JOptionPane.showMessageDialog(null, "nobody cares what you think");
+	}
 }
 }
